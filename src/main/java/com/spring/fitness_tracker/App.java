41714af21e113e9bf140e203f45cfe3933e5ca5c
Hemant Sethi <sethi.hemant@gmail.com>
2018-03-17 16:25:46 -0400

    Adding the base version of the project to implement spring security.

---
+++ src/main/java/com/spring/fitness_tracker/App.java
@@ -0,0 +1,13 @@
+package com.spring.fitness_tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
